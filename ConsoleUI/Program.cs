cc20ca86eeb368de4730dcec302a16155950bff0
Birkankaraer <karaermustafabirkan@gmail.com>
2023-09-15 15:51:01 +0300

    15.09.2023 Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,10 +5,12 @@ using DataAccess.Concrete.InMemory;
 
 internal class Program
 {
+    //SOLID 
+    //Open Closed Principle
     private static void Main(string[] args)
     {
         ProductManager productManager = new ProductManager(new EfProductDal());
-        foreach (var product in productManager.GetAll())
+        foreach (var product in productManager.GetByUnitPrice(40,100))
         {
             Console.WriteLine(product.ProductName);
         }
--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -9,7 +9,7 @@ namespace Entities.Concrete
 {
     public class Customer : IEntity
     {
-        public string CsutomerId { get; set; }
+        public string CustomerId { get; set; }
         public string ContactName { get; set; }
         public string CompanyName { get; set; }
         public string City { get; set; }
