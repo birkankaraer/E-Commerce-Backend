fda5e5fdfc3271da713091a5c9b22281507f3e6c
Birkankaraer <karaermustafabirkan@gmail.com>
2023-09-15 14:30:50 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        foreach (var product in productManager.GetAll())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+        
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    /*Çıplak Class Kalmasın inheritance, Interface Imp. almıyorsa ilerde sıkıntı yaşanır demektir.
+    Classın Category classı olması gibi*/
+    public class Category : IEntity
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
