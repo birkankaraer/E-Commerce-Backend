bd6a706184d23e06c6a8a6f61a3cfae83097e146
Birkankaraer <karaermustafabirkan@gmail.com>
2023-09-23 15:57:37 +0300

    Updates

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -20,6 +21,13 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business rules codes
+            _productDal.Add(product);
+            return new Result(true, "Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             //İş kodları
@@ -30,6 +38,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p=>p.CategoryId == id);
+        }
+
+        public Product GetById(int ProductId)
+        {
+            return _productDal.Get(p=>p.ProductId == p.ProductId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
