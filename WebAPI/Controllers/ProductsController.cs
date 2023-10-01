72a5329f5235406dc500767fd94bf0464a4e6a5d
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-01 23:58:46 +0300

    Update

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product {ProductId = 1, ProductName = "Elma"},
+                new Product {ProductId = 2, ProductName = "Armut"},
+            };
+        }
+        
+    }
+}
